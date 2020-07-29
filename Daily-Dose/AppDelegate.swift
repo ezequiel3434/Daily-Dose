a97ff292d684b968e5c7cce84a87396ee14ebf4e
ezequiel3434 <ezequiel3434@gmail.com>
2020-07-29 19:33:27 -0300

    bug fixed

--- Daily-Dose/AppDelegate.swift
+++ Daily-Dose/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        GADMobileAds.configure(withApplicationID: "ca-app-pub-3940256099942544~1458002511")
+        GADMobileAds.sharedInstance().start(completionHandler: nil)
         
         return true
     }
